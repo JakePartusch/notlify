e9a14675971a179c352507890adf61617f0c2211
Jake Partusch <jakepartusch@gmail.com>
2022-12-20 21:58:40 -0600

    feat: adding apollo

--- src/control-plane/api.ts
+++ src/control-plane/api.ts
@@ -1,3 +1,24 @@
-export const handler = async (event) => {
-  console.log(JSON.stringify(event, null, 2));
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda"; //highlight-line
+
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
 };
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  includeStacktraceInErrorResponses: true,
+});
+
+// This final export is important!
+export const handler = startServerAndCreateLambdaHandler(server); //highlight-line
