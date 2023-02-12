0012280025f0ff24e9a829facf1e2aec95c6548b
Jake Partusch <jakepartusch@gmail.com>
2023-02-11 22:49:17 -0600

    feat: adding last deployment time

--- src/control-plane/ui/app/src/pages/index.tsx
+++ src/control-plane/ui/app/src/pages/index.tsx
@@ -31,6 +31,34 @@ const allApps = graphql(/* GraphQL */ `
   }
 `);
 
+function timeAgo(value: string) {
+  const seconds = Math.floor(
+    (new Date().getTime() - new Date(value).getTime()) / 1000
+  );
+  let interval = seconds / 31536000;
+  const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
+  if (interval > 1) {
+    return rtf.format(-Math.floor(interval), "year");
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return rtf.format(-Math.floor(interval), "month");
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return rtf.format(-Math.floor(interval), "day");
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return rtf.format(-Math.floor(interval), "hour");
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return rtf.format(-Math.floor(interval), "minute");
+  }
+  return rtf.format(-Math.floor(interval), "second");
+}
+
 const navigation = [{ name: "Dashboard", href: "#", current: true }];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
@@ -450,12 +478,19 @@ export default function Dashboard() {
                             </a>
                           )}
                         </p>
+
                         <p className="flex space-x-2 text-sm text-gray-500">
                           <span>Next.js</span>
                           <span aria-hidden="true">&middot;</span>
-                          {/*@ts-ignore */}
-                          <span>Last deploy {application.lastDeploy}</span>
-                          <span aria-hidden="true">&middot;</span>
+                          {application.lastDeploymentTime && (
+                            <>
+                              <span>
+                                Last deploy{" "}
+                                {timeAgo(application.lastDeploymentTime)}
+                              </span>
+                              <span aria-hidden="true">&middot;</span>
+                            </>
+                          )}
                           <span>All Edge Locations</span>
                         </p>
                       </div>
