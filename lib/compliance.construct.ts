6b0612d14098a1a4fd417e116cb2fef55e743956
Jake Partusch <jakepartusch@gmail.com>
2023-02-21 19:31:35 -0600

    fix: extra curly in cf function

--- lib/compliance.construct.ts
+++ lib/compliance.construct.ts
@@ -42,7 +42,7 @@ export class Compliance extends Construct {
             }
             return request;
         }
-         }`),
+        `),
       }
     );
     this.complianceCloudfrontFunction = complianceFunction;
