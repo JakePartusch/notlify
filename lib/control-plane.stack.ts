f1d82a5b409d9b73052858168e61cf10077a32b9
Jake Partusch <jakepartusch@gmail.com>
2023-06-20 20:50:07 -0500

    feat: updating control plane to use sst (#1)
    * feat: updating control plane to use sst
    
    * feat: update workflow

--- lib/control-plane.stack.ts
+++ lib/control-plane.stack.ts
@@ -1,262 +1,136 @@
-import {
-  HttpApi,
-  CorsPreflightOptions,
-  CorsHttpMethod,
-} from "@aws-cdk/aws-apigatewayv2-alpha";
-import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
-import * as cdk from "aws-cdk-lib";
-import {
-  CfnOutput,
-  Duration,
-  RemovalPolicy,
-  Stack,
-  StackProps,
-} from "aws-cdk-lib";
-import {
-  AttributeType,
-  BillingMode,
-  ProjectionType,
-  Table,
-} from "aws-cdk-lib/aws-dynamodb";
-import {
-  CfnEventBus,
-  CfnEventBusPolicy,
-  EventBus,
-  Rule,
-} from "aws-cdk-lib/aws-events";
-import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
-import {
-  Effect,
-  ManagedPolicy,
-  PolicyStatement,
-  Role,
-  ServicePrincipal,
-} from "aws-cdk-lib/aws-iam";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { BlockPublicAccess, Bucket, EventType } from "aws-cdk-lib/aws-s3";
-import { LambdaDestination } from "aws-cdk-lib/aws-s3-notifications";
-import { Construct } from "constructs";
-import * as path from "path";
-import { LambdaAlarm } from "./lambda-alarm.construct";
+import { CfnOutput } from "aws-cdk-lib";
+import { CfnEventBusPolicy } from "aws-cdk-lib/aws-events";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import * as sst from "sst/constructs";
 
-interface ControlPlaneStackProps extends StackProps {
+interface ControlPlaneStackProps {
   gitHubToken: string;
   gitHubWorkflowUrl: string;
   dataPlaneAccounts: string[];
   orgId: string;
 }
 
-export class ControlPlaneStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: ControlPlaneStackProps) {
-    super(scope, id, props);
+const { GH_TOKEN, GH_WORKFLOW_URL, DATA_PLANE_ACCOUNTS, ORG_ID } = process.env;
 
-    const table = new Table(this, "ControlPlaneTable", {
-      partitionKey: {
-        name: "PK",
-        type: AttributeType.STRING,
-      },
-      sortKey: {
-        name: "SK",
-        type: AttributeType.STRING,
-      },
-      pointInTimeRecovery: true,
-      billingMode: BillingMode.PAY_PER_REQUEST,
-    });
+export function ControlPlaneStack(ctx: sst.StackContext) {
+  const { stack } = ctx;
 
-    table.addGlobalSecondaryIndex({
-      indexName: "GSI1",
-      partitionKey: {
-        name: "GSI1PK",
-        type: AttributeType.STRING,
-      },
-      sortKey: {
-        name: "GSI1SK",
-        type: AttributeType.STRING,
-      },
-      projectionType: ProjectionType.ALL,
-    });
+  const props: ControlPlaneStackProps = {
+    gitHubToken: GH_TOKEN!,
+    gitHubWorkflowUrl: GH_WORKFLOW_URL!,
+    dataPlaneAccounts: DATA_PLANE_ACCOUNTS!.split(","),
+    orgId: ORG_ID!,
+  };
 
-    const sourceFilesBucket = new Bucket(this, "SourceFilesBucket", {
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-    });
-
-    const lambda = new NodejsFunction(this, "ControlPlaneApiHandler", {
-      entry: path.join(__dirname, "../src/control-plane/api.ts"),
-      runtime: Runtime.NODEJS_18_X,
-      memorySize: 512,
-      timeout: Duration.seconds(30),
-      environment: {
-        TABLE_NAME: table.tableName,
-        SOURCE_FILES_BUCKET_NAME: sourceFilesBucket.bucketName,
-        GITHUB_TOKEN: props.gitHubToken,
-        GITHUB_WORKFLOW_URL: props.gitHubWorkflowUrl,
-      },
-    });
-
-    new LambdaAlarm(this, "ControlPlaneApiAlarm", {
-      emailRecipient: "jakepartusch@gmail.com",
-      lambdaFunction: lambda,
-    });
-
-    const httpApi = new HttpApi(this, "HttpApi", {
-      corsPreflight: {
-        allowHeaders: ["*"],
-        allowMethods: [CorsHttpMethod.ANY],
-        allowOrigins: ["*"],
-        maxAge: Duration.seconds(600),
-      },
-    });
-    const lambdaProxyIntegration = new HttpLambdaIntegration(
-      `LambdaIntegration`,
-      lambda
-    );
-
-    httpApi.addRoutes({
-      path: `/api`,
-      integration: lambdaProxyIntegration,
-    });
-
-    new CfnOutput(this, `API Gateway`, {
-      value: `https://${httpApi.apiId}.execute-api.${
-        Stack.of(this).region
-      }.amazonaws.com/api`,
-    });
-
-    const s3NotifyLambdaRole = new Role(
-      this,
-      "DeploymentInitiatedHandlerHandlerRole",
-      {
-        assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
-        managedPolicies: [
-          ManagedPolicy.fromAwsManagedPolicyName(
-            "service-role/AWSLambdaBasicExecutionRole"
-          ),
-        ],
-      }
-    );
-    for (const account of props.dataPlaneAccounts) {
-      s3NotifyLambdaRole.addToPolicy(
+  const dataPlaneAssumePolicyStatements: PolicyStatement[] =
+    props.dataPlaneAccounts.map(
+      (account) =>
         new PolicyStatement({
           actions: ["sts:AssumeRole"],
           effect: Effect.ALLOW,
           resources: [`arn:aws:iam::${account}:role/*`],
         })
-      );
-    }
-    sourceFilesBucket.grantRead(s3NotifyLambdaRole);
+    );
 
-    const s3NotifyLambda = new NodejsFunction(
-      this,
-      "DeploymentInitiatedHandler",
-      {
-        entry: path.join(
-          __dirname,
-          "../src/control-plane/lambdas/deployment-initiated.handler.ts"
-        ),
-        runtime: Runtime.NODEJS_18_X,
-        memorySize: 512,
-        timeout: Duration.seconds(60),
-        role: s3NotifyLambdaRole,
+  const table = new sst.Table(stack, "ControlPlaneTable", {
+    fields: {
+      PK: "string",
+      SK: "string",
+      GSI1PK: "string",
+      GSI1SK: "string",
+    },
+    primaryIndex: { partitionKey: "PK", sortKey: "SK" },
+    globalIndexes: {
+      GSI1: { partitionKey: "GSI1PK", sortKey: "GSI1SK" },
+    },
+  });
+
+  const sourceFilesBucket = new sst.Bucket(stack, "SourceFilesBucket", {});
+
+  sourceFilesBucket.addNotifications(stack, {
+    deploymetnInitiated: {
+      function: {
+        handler:
+          "src/control-plane/lambdas/deployment-initiated.handler.handler",
+        permissions: [sourceFilesBucket, table],
         environment: {
           TABLE_NAME: table.tableName,
           SOURCE_FILES_BUCKET_NAME: sourceFilesBucket.bucketName,
           GITHUB_TOKEN: props.gitHubToken,
           GITHUB_WORKFLOW_URL: props.gitHubWorkflowUrl,
         },
-      }
-    );
-
-    sourceFilesBucket.enableEventBridgeNotification();
-
-    sourceFilesBucket.addEventNotification(
-      EventType.OBJECT_CREATED,
-      new LambdaDestination(s3NotifyLambda)
-    );
-
-    sourceFilesBucket.grantWrite(lambda);
-    table.grantReadWriteData(lambda);
-    table.grantReadWriteData(s3NotifyLambda);
-
-    new LambdaAlarm(this, "DeploymentInitiatedHandlerAlarm", {
-      emailRecipient: "jakepartusch@gmail.com",
-      lambdaFunction: s3NotifyLambda,
-    });
-
-    const eventbus = new CfnEventBus(this, "CloudformationEventBus", {
-      name: "CloudformationEventBus",
-    });
-
-    const eventBusPolicy = new CfnEventBusPolicy(this, "EventBusPolicy", {
-      eventBusName: "CloudformationEventBus",
-      statementId: "AllowPutEventsWithinOrganizationAccounts",
-      action: "events:PutEvents",
-      principal: "*",
-      condition: {
-        key: "aws:PrincipalOrgID",
-        type: "StringEquals",
-        value: props.orgId,
       },
-    });
-    eventBusPolicy.addDependsOn(eventbus);
+      events: ["object_created"],
+    },
+  });
 
-    const deploymentProgressHandlerRole = new Role(
-      this,
-      "DeploymentProgressHandlerRole",
-      {
-        assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
-        managedPolicies: [
-          ManagedPolicy.fromAwsManagedPolicyName(
-            "service-role/AWSLambdaBasicExecutionRole"
-          ),
-        ],
-      }
-    );
-    for (const account of props.dataPlaneAccounts) {
-      deploymentProgressHandlerRole.addToPolicy(
-        new PolicyStatement({
-          actions: ["sts:AssumeRole"],
-          effect: Effect.ALLOW,
-          resources: [`arn:aws:iam::${account}:role/*`],
-        })
-      );
-    }
-    table.grantReadWriteData(deploymentProgressHandlerRole);
-    const deploymentProgressHandler = new NodejsFunction(
-      this,
-      "DeploymentProgressHandler",
-      {
-        entry: path.join(
-          __dirname,
-          "../src/control-plane/lambdas/deployment-progress.handler.ts"
-        ),
-        runtime: Runtime.NODEJS_18_X,
-        memorySize: 512,
-        timeout: Duration.seconds(60),
-        role: deploymentProgressHandlerRole,
-        environment: {
-          TABLE_NAME: table.tableName,
+  const api = new sst.Api(stack, "Api", {
+    cors: {
+      allowHeaders: ["*"],
+      allowMethods: ["ANY"],
+      allowOrigins: ["*"],
+      maxAge: "600 seconds",
+    },
+    routes: {
+      "ANY /api": {
+        function: {
+          handler: "src/control-plane/api.handler",
+          timeout: 30,
+          permissions: [
+            sourceFilesBucket,
+            table,
+            ...dataPlaneAssumePolicyStatements,
+          ],
+          environment: {
+            TABLE_NAME: table.tableName,
+            SOURCE_FILES_BUCKET_NAME: sourceFilesBucket.bucketName,
+            GITHUB_TOKEN: props.gitHubToken,
+            GITHUB_WORKFLOW_URL: props.gitHubWorkflowUrl,
+          },
         },
-      }
-    );
-    const cloudformationEventBus = EventBus.fromEventBusName(
-      this,
-      "CloudformationEventBusRef",
-      "CloudformationEventBus"
-    );
-    new Rule(this, "CloudformationAggregatorRule", {
-      eventBus: cloudformationEventBus,
-      eventPattern: {
-        source: ["aws.cloudformation"],
       },
-      targets: [new LambdaFunction(deploymentProgressHandler)],
-    });
+    },
+  });
 
-    new LambdaAlarm(this, "DeploymentProgressHandlerAlarm", {
-      emailRecipient: "jakepartusch@gmail.com",
-      lambdaFunction: deploymentProgressHandler,
-    });
-  }
+  new CfnOutput(stack, `API Gateway`, {
+    value: `${api.url}/api`,
+  });
+
+  new sst.EventBus(stack, "SstCloudformationEventBus", {
+    cdk: {
+      eventBus: {
+        eventBusName: `CloudformationEventBus-${stack.stage}`,
+      },
+    },
+    rules: {
+      cloudformationRule: {
+        pattern: { source: ["aws.cloudformation"] },
+        targets: {
+          deploymentProgressHandler: {
+            function: {
+              handler:
+                "src/control-plane/lambdas/deployment-progress.handler.handler",
+              environment: {
+                TABLE_NAME: table.tableName,
+              },
+              timeout: 60,
+              permissions: [table, ...dataPlaneAssumePolicyStatements],
+            },
+          },
+        },
+      },
+    },
+  });
+
+  new CfnEventBusPolicy(stack, "EventBusPolicy", {
+    eventBusName: `CloudformationEventBus-${stack.stage}`,
+    statementId: `AllowPutEventsWithinOrganizationAccounts-${stack.stage}`,
+    action: "events:PutEvents",
+    principal: "*",
+    condition: {
+      key: "aws:PrincipalOrgID",
+      type: "StringEquals",
+      value: props.orgId,
+    },
+  });
 }
